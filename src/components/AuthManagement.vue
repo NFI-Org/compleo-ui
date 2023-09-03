d3116279d7469496587a9b537a507ab008b013d1
Punyasloka Parida <12557052+bitsnorbytes@users.noreply.github.com>
2023-09-03 22:04:32 +0530

    Typewriter

--- src/components/AuthManagement.vue
+++ src/components/AuthManagement.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { handleLogin } from '../utils/useAuth'
+import { handleLogin,handleSignup,isSignUpSuccess } from '../utils/useAuth'
 
 const email = ref('')
 const password = ref('')
@@ -8,21 +8,14 @@ const password = ref('')
 </script>
 
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-white">
-    <body class="h-full">
-    ```
-  -->
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-10 w-auto" src="../assets/logo/compleo.svg" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+      <h2 v-if="isSignUpSuccess === true" class="mt-10 text-center text-xl font-bold leading-9 tracking-tight text-gray-900">Please verify your email through the link sent to your inbox to finish the sign up process.</h2>
+      <h2 v-else class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
     </div>
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <div v-if="isSignUpSuccess === false" class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6"  @submit.prevent="handleLogin({ email, password })">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
@@ -50,12 +43,14 @@ const password = ref('')
           />
         </div>
       </form>
-
-      <p class="mt-10 text-center text-sm text-gray-500">
-        Not a member?
-        {{ ' ' }}
-        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-      </p>
+      <form class="m-6 space-y-6"  @submit.prevent="handleSignup({ email, password })">
+      <div>
+          <input type="submit" 
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          value="Not a member? Sign up"
+          />
+        </div>
+      </form>
     </div>
   </div>
 </template>
